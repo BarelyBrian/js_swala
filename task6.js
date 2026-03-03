be6803ee1babc0f88cb039e1ca8ddd8365135dd2
BarelyBrian <brian.airbnb.bb@gmail.com>
2026-03-03 16:31:29 +0300

    t

--- task6.js
+++ task6.js
@@ -1,6 +1,6 @@
 // Create an arrow function called fizzBuzz that takes one parameter called num, checks the value of num and prints "Fizz" if the number is divisible by 3 and "Buzz" if it is divisible by 5. 
 const fizzBuzz = (num) =>{
-    if (num%15 ==0){
+    if (num%5 ==0 && num%3==0){
         console.log("FizzBuzz")
     }
     else if (num%5 ==0){
